8216deb8e0955c4fe708f0d968ed2574e5cbe83e
shaibaz <shaibazansarii.48@gmail.com>
2021-03-10 15:41:16 +0530

    change db

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const app = require("./app");
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 // const DB = process.env.DATABASE_LOCAL;
 mongoose
-    // .connect(process.env.DATABASE_LOCAL, {   //local database k liye 
-    .connect(DB, {
+    .connect(process.env.DATABASE_LOCAL, {   //local database k liye 
+    // .connect(DB, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
